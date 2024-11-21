296cf4f119513858a08a55529fb06621aefd6ee9
Rise-KuN <40178259+Rise-KuN@users.noreply.github.com>
2024-11-21 17:21:53 +0100

    Update adawet_all_in_one.lua to 1.4.3

--- All in one/adawet_all_in_one.lua
+++ All in one/adawet_all_in_one.lua
@@ -1,7 +1,7 @@
 script_name = "أدوات"
 script_description = "أدوات متعددة الاستخدام"
 script_author = "Rise-KuN"
-script_version = "1.4.2"
+script_version = "1.4.3"
 
 include("unicode.lua")
 local json = require 'json'
@@ -503,6 +503,53 @@ function adjust_clips(subtitles, selected_lines, active_line)
     end
 
     -- aegisub.set_undo_point("Adjust Clip Positions (" .. selected_direction .. ")")
+end
+
+-- تقسيم السطر إلى فريمات
+-- Calculate the video frame rate
+function calculate_fps()
+    local start_ms = 0  -- Starting time in milliseconds
+    local end_ms = 1000 -- End time in milliseconds (1 second)
+
+    local start_frame = aegisub.frame_from_ms(start_ms)
+    local end_frame = aegisub.frame_from_ms(end_ms)
+
+    -- Number of frames in one second is the frame rate
+    return end_frame - start_frame
+end
+
+function split_line_to_frames(subtitles, selected_lines)
+    aegisub.progress.task("Splitting line to frames...")
+
+    -- Default frame rate
+    -- local frame_rate = 23.976
+    local frame_rate = calculate_fps()
+    -- aegisub.debug.out("frame rate: " .. frame_rate .. " fps\n")
+	
+	-- Frame duration in milliseconds
+    local frame_duration = 1000 / frame_rate
+
+    for _, i in ipairs(selected_lines) do
+        local line = subtitles[i]
+        local start_time = line.start_time
+        local end_time = line.end_time
+        local duration = end_time - start_time
+
+        -- Calculate the number of frames
+        local num_frames = math.floor(duration / frame_duration)
+
+        for frame = 0, num_frames - 1 do
+            local new_line = table.copy(line)
+            new_line.start_time = start_time + frame * frame_duration
+            new_line.end_time = math.min(new_line.start_time + frame_duration, end_time)
+            subtitles.insert(i + frame, new_line)
+        end
+
+        -- Remove the original line
+        subtitles.delete(i + num_frames)
+    end
+
+    aegisub.set_undo_point(script_name)
 end
 
 -- حساب نسبة التقدم
@@ -1147,9 +1194,10 @@ function escape_lua_pattern(str)
     return str:gsub("([%.%^%$%(%)%[%]%%%+%-%?])", "%%%1")
 end
 
-aegisub.register_macro("أدوات/10 - حساب نسبة التقدم", "حساب نسبة التقدم", calculate_progress)
-aegisub.register_macro("أدوات/09 - حذف ما بين الكلمات", "حذف ما بين الكلمات", remove_text_between_characters)
-aegisub.register_macro("أدوات/08 - أداة الحذف", "أداة الحذف", remove_tool)
+aegisub.register_macro("أدوات/11 - حساب نسبة التقدم", "حساب نسبة التقدم", calculate_progress)
+aegisub.register_macro("أدوات/10 - حذف ما بين الكلمات", "حذف ما بين الكلمات", remove_text_between_characters)
+aegisub.register_macro("أدوات/09 - أداة الحذف", "أداة الحذف", remove_tool)
+aegisub.register_macro("أدوات/08 - تقسيم السطر إلى فريمات", "تقسيم السطر إلى فريمات", split_line_to_frames)
 aegisub.register_macro("أدوات/07 - تغيير موضع الكليب", "تغيير موضع الكليب", adjust_clips)
 aegisub.register_macro("أدوات/06 - تغيير اتجاه النص", "تغيير اتجاه النص", reverse_text_direction)
 aegisub.register_macro("أدوات/05 - تصحيح نقاط آخر السطر", "تصحيح نقاط آخر السطر", fix_punctuation)
